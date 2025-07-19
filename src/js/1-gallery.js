27f5df010936a7bf4ab7c037ff20d310b7a21e35
tokareva-stacy <tokarevanyura@gmail.com>
2025-07-19 19:55:56 +0300

    +

---
+++ src/js/1-gallery.js
@@ -0,0 +1,68 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const images = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rattlesnake-4200292__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rattlesnake-4200292_1280.jpg",
+    description: "Rattlesnake",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Beach",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower bouquet",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546_1280.jpg",
+    description: "Scope",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/ocean-4206786__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/ocean-4206786_1280.jpg",
+    description: "Ocean",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546_1280.jpg",
+    description: "Scope",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546_1280.jpg",
+    description: "Scope",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/19/43/scope-4200546_1280.jpg",
+    description: "Scope",
+  },
+];
